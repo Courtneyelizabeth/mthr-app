f5117952c66f56301e0a9592c96021ba8f12a087
Courtney Maxwell <courtneymaxwell@Courtneys-MacBook-Pro.local>
2026-03-27 10:47:36 -0600

    add tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,25 +9,21 @@ const config: Config = {
   theme: {
     extend: {
       fontFamily: {
-        bebas: ['var(--font-bebas)', 'sans-serif'],
         cormorant: ['var(--font-cormorant)', 'serif'],
         dm: ['var(--font-dm)', 'sans-serif'],
       },
       colors: {
         mthr: {
-          white:  '#FAFAF8',
-          off:    '#F2F0ED',
+          white:  '#FFFFFF',
+          off:    '#F5F2EE',
+          bg:     '#EDE9E3',
           b1:     '#E8E4DE',
           b2:     '#D0CCC6',
-          mid:    '#888480',
+          mid:    '#8A8680',
           dim:    '#C0BCB6',
           dark:   '#2A2620',
-          black:  '#0D0C0A',
+          black:  '#1A1814',
         },
-      },
-      letterSpacing: {
-        wider2: '0.14em',
-        widest2: '0.2em',
       },
     },
   },
